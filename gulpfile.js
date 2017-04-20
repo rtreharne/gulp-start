ba58690d95cefe6d96c1821ea2d551cd0d73dd22
Rob Treharne <rob.treharne@gmail.com>
2017-04-20 21:08:16 +0100

    configured to livereload!

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,39 @@
 var gulp = require('gulp'),
     jade = require('gulp-jade'),
     sass = require('gulp-sass'),
+    livereload = require('gulp-livereload'),
     webserver = require('gulp-webserver');
+
+gulp.task("watch", function(){
+    livereload.listen();
+    gulp.watch('sass/**/*.sass', ['sass']);
+    gulp.watch('jade/**/*.jade', ['jade']);
+})
 
 // Compile and copy Jade
 gulp.task("jade", function(event) {
     return gulp.src('jade/**/*.jade')
     .pipe(jade({pretty: true}))
     .pipe(gulp.dest(''))
+    .pipe(livereload());
 });
 
 // Compile and copy Sass
 gulp.task("sass", function(){
-    return gulp.src('./sass/**/*.scss')
+    return gulp.src('sass/**/*.sass')
       .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('./css'));
+      .pipe(gulp.dest('./css'))
+      .pipe(livereload());
 });
 
 // Start webserver
 gulp.task("webserver", function(){
-    gulp.src('')
+    gulp.src('.')
       .pipe(webserver({
         livereload: true,
         directoryListing: true,
         open: true
     }));
 });
+
+gulp.task("default", ["jade", "sass", "watch", "webserver"]);
